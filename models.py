e2e3c0941c508931c4ec54b1a359d8a6c83181fd
Surya Kasturi <suryak@udel.edu>
2019-08-08 00:12:28 -0400

    update

--- models.py
+++ models.py
@@ -9,6 +9,57 @@ import torch.nn as nn
 import torch
 
 
+
+class StatefulMultiheadAttention(nn.MultiheadAttention):
+
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.saved_state = {}
+
+    def _get_input_buffer(self, incremental_state):
+        return self.saved_state
+
+    def _set_input_buffer(self, incremental_state, saved_state):
+        self.saved_state = saved_state
+
+    def reset_input_buffer(self):
+        self.saved_state = {}
+
+
+class DialogContext(nn.Module):
+
+    def __init__(self, emb_size):
+        super().__init__()
+        # https://arxiv.org/pdf/1706.03762.pdf
+        self.l0 = StatefulMultiheadAttention(emb_size, 2)
+
+    def reset_input_buffer(self):
+        self.l0.reset_input_buffer()
+
+    def forward(self, utter_emb, desc_emb):
+        # query [Tokens, Batch, Emb]
+        utter_emb = utter_emb.permute(1,0,2)
+
+        # key and values [Service * Tokens, Batch, Emb]
+        desc_emb = torch.flatten(desc_emb, 1, 2)
+        desc_emb = desc_emb.permute(1,0,2)
+
+        # output [Tokens, Batch, Emb]
+        # weights [Batch, Tokens, Service * Tokens]
+        output, weights = self.l0(
+            utter_emb, 
+            desc_emb, 
+            desc_emb, 
+            incremental_state=True,
+            need_weights=True,
+        )
+
+        # change shapes
+        output = output.permute(1,0,2)
+
+        return output
+
+
 class UserIntentPredictor(Model):
 
     def __init__(self, vocab):
@@ -18,42 +69,69 @@ class UserIntentPredictor(Model):
         # layers
         self.l0 = nn.Linear(self.emb.output_dim, self.emb.output_dim)
         self.l1 = nn.Linear(self.emb.output_dim, self.emb.output_dim)
-        self.l2 = nn.Linear(1, 1)
+        #self.l2 = DialogContext(self.emb.output_dim)
         # metrics
         self.accuracy = BooleanAccuracy()
+
+        # init weights
+        for name, param in self.named_parameters():
+            if not name.startswith("emb."):
+                param.data.fill_(0)
+
+    def reset_context(self):
+        self.l2.reset_input_buffer()
 
     def get_metrics(self, reset):
         return {"accuracy": self.accuracy.get_metric(reset)}
 
-    def forward(self, turnid, batch):
+    def forward(self, **batch):
+        turnid = int(batch["turnid"])
+        serviceid = int(batch["serviceid"])
+
         # encode utter [Batch, Tokens, Emb]
         utter = self.emb(batch["usr_utter"]["tokens"][:,turnid,:])
         utter = self.l0(utter)
 
-        # encode intent desc [Batch, Service, Intent, Tokens, Emb]
+        # encode intent desc [Batch, Intent, Tokens, Emb]
         intent_desc = self.emb(batch["intent_desc"]["tokens"])
+        intent_desc = intent_desc[:,serviceid,:]
         intent_desc = self.l1(intent_desc)
 
-        # compute prediction score
-        # see https://github.com/allenai/allennlp/issues/2668
-        mask_usr_utter = (batch["usr_utter"]["tokens"][:,turnid,:] != 0).float()
-        mask_intent_desc = (batch["intent_desc"]["tokens"] != 0).float()
-        
-        score = torch.einsum(
-            "bxe,bsiye,bx,bsiy->bsi", 
-            utter, intent_desc, mask_usr_utter, mask_intent_desc
-        ) # [B, S, I]
-        score = self.l2(score.unsqueeze(-1)).squeeze(-1) # [B, S, I]
-        score = torch.sigmoid(score)
-        labels = torch.argmax(score, -1).float()
+        # [Batch, Tokens, Emb]
+        #context = self.l2(utter, intent_desc)
 
-        output = {"score": score}
+        # see https://github.com/allenai/allennlp/issues/2668
+        mask_utter = (batch["usr_utter"]["tokens"][:,turnid,:] != 0).float()
+        mask_intent_desc = (batch["intent_desc"]["tokens"][:,serviceid,:] != 0).float()
+        
+        # ranking on intents at each service
+        score = torch.einsum(
+            "bxe,bx,biye,biy->bi", 
+            utter, mask_utter,
+            intent_desc, mask_intent_desc,
+        )
+        score = torch.sigmoid(score) # [B, I]
+        labels = torch.argmax(score, -1) # [B,]
+        output = {"score": score, "labels": labels}
+
         if "intent_exist" in batch:
-            target_score = batch["intent_exist"][:, turnid, ...] # [Batch, Service, Intent]
-            target_mask = target_score.sum(-1) # [Batch, Service] if the onehot encoding has all zeros
-            target_labels = torch.argmax(target_score, -1).float()
-            self.accuracy(labels, target_labels, target_mask)
-            output["loss"] = F.mse_loss(score, target_score)
+            # target values
+            target_score = batch["intent_exist"][:,turnid,serviceid,:] # [Batch, Intent]
+            values, target_labels = torch.max(target_score, -1) # [Batch,]
+
+            # update the accuracy counters. reset at every dialogue
+            self.accuracy(
+                labels.float(), 
+                target_labels.float(), 
+                mask=(values != -1).float(),
+            )
+
+            # calculate loss
+            output["loss"] = F.binary_cross_entropy(
+                score,
+                target_score,
+                weight=(target_score != -1).float(), # passing mask as weights
+            )
 
         return output
     
--- train.py
+++ train.py
@@ -1,63 +1,72 @@
-from allennlp.data import Vocabulary
-from allennlp.data.iterators import BucketIterator, BasicIterator
-from allennlp.nn.util import move_to_device
-
 import torch
-import logging
+import math
 
-from models import UserIntentPredictor
-from readers import DialogueReader
+from allennlp.common.util import is_lazy, ensure_list
 
+class DialogIterator(object):
+    """Unfold a batch of dialogues."""
 
-def to_model(obj):
-    if isinstance(obj, torch.nn.DataParallel):
-        return obj.module
-    return obj
+    def __init__(self, iterator):
+        self.iterator = iterator
+    
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return getattr(self, attr)
+        return getattr(self.iterator, attr)
 
+    def get_num_batches(self, instances):
+        # modified allennlp implementation
+        if is_lazy(instances) and self._instances_per_epoch is None:
+            return 1
+        elif self._instances_per_epoch is not None:
+            return math.ceil(self._instances_per_epoch / self._batch_size)
+        else:
+            batches = 0
+            for dial in ensure_list(instances):
+                batches += dial.fields["num_turns"].as_tensor({})
+                batches += len(dial.fields["service"].metadata)
+            return math.ceil(batches / self._batch_size)
 
-def trainer(model, optim, iterator, train_dataset, num_epochs, device, print_every):
-    counter = 0
-    model = model.to(device)
-    model = torch.nn.DataParallel(model)
-    model.train()
-    counter = 0
-    for epoch in range(num_epochs):
-        for batch in iterator(train_dataset):
-            counter += 1
-            batch = move_to_device(batch, 0)
+    def __call__(self, *args, **kw):
+        for batch in self.iterator(*args, **kw):
             num_turns = batch["usr_utter"]["tokens"].shape[1]
-            metrics = {"loss": 0, "acc": 0}
+            num_services = batch["service_desc"]["tokens"].shape[1]
             for turnid in range(num_turns):
-                optim.zero_grad()
-                output = model(turnid, batch)
-                output["loss"] = output["loss"].mean()
-                output["loss"].backward()
-                metrics["loss"] += output["loss"].item()
-            metrics["acc"] = to_model(model).get_metrics(reset=True)["accuracy"]
-            if counter % print_every == 0:
-                print("Iteration: {}, Epoch: {}, Loss: {}, Acc: {}".format(
-                    epoch, counter, metrics["loss"], metrics["acc"]))
+                for sid in range(num_services):
+                    inputs = dict(turnid=turnid, serviceid=sid)
+                    inputs.update(batch)
+                    yield inputs
 
 
 if __name__ == "__main__":
     # $ CUDA_VISIBLE_DEVICES=0,1,2,3 nohup python -u train.py &
+    from allennlp.data.iterators import BasicIterator
+    from allennlp.training import Trainer
+    from models import UserIntentPredictor
+    from readers import DialogueReader
+    import os
+
+    allen_device = [int(i) for i in os.environ.get("CUDA_VISIBLE_DEVICES", "-1").split(",")]
+    torch_device = "cuda" if allen_device[0] != -1 else "cpu"
+
     print("loading training data")
     train_ds = torch.load("data/preprocessed/dataset.pkl")
     vocab = torch.load("data/preprocessed/vocab.pkl")
 
     print("loading model")
-    model = UserIntentPredictor(vocab)
+    model = UserIntentPredictor(vocab).to(torch_device)
     optim = torch.optim.Adam(model.parameters(), lr=0.001)
     iterator = BasicIterator(batch_size=32)
     iterator.index_with(vocab)
 
     print("started training")
-    trainer(
+    trainer = Trainer(
         model=model,
-        optim=optim,
-        iterator=iterator,
+        optimizer=optim,
+        iterator=DialogIterator(iterator),
         train_dataset=train_ds,
-        num_epochs=10,
-        device="cuda",
-        print_every=10
+        num_epochs=2,
+        cuda_device=allen_device,
     )
+
+    trainer.train()
